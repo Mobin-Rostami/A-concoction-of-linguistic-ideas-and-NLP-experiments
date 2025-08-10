b9eecde12d250672bbc1c362213f2dca8fd9ba9a
Mobin Rostami Chavoshlou <chavoshlou@gmail.com>
2025-08-10 20:10:43 +0330

    Update 05_lemmatizer.py
    Lemmatization is a natural language processing technique that reduces words to their base or dictionary form, known as the lemma. It's a process of grouping together inflected forms of a word (like "running," "ran," and "runs") under a single lemma ("run"). Unlike stemming, which simply chops off affixes, lemmatization considers the word's context and part of speech to determine the most accurate base form.

--- scripts/05_lemmatizer.py
+++ scripts/05_lemmatizer.py
@@ -1,1 +1,9 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+text = "The striped cats were chasing mice."
+doc = nlp(text)
+
+for token in doc:
+    print(f"{token.text:<15}{token.lemma_}")
 
